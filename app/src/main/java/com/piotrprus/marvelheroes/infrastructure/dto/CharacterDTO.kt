e5ca1412422a6e416408dcc87b5ef50e596f5292
Piotr Prus <prus.piotr@gmail.com>
2022-07-22 18:27:36 +0100

    Change character ID to Int type, according to marvel documentation

--- app/src/main/java/com/piotrprus/marvelheroes/infrastructure/dto/CharacterDTO.kt
+++ app/src/main/java/com/piotrprus/marvelheroes/infrastructure/dto/CharacterDTO.kt
@@ -2,7 +2,7 @@ package com.piotrprus.marvelheroes.infrastructure.dto
 
 @kotlinx.serialization.Serializable
 data class CharacterDTO(
-    val id: Long,
+    val id: Int,
     val name: String,
     val description: String,
     val modified: String,
--- app/src/main/java/com/piotrprus/marvelheroes/ui/model/CharacterItem.kt
+++ app/src/main/java/com/piotrprus/marvelheroes/ui/model/CharacterItem.kt
@@ -1,7 +1,7 @@
 package com.piotrprus.marvelheroes.ui.model
 
 data class CharacterItem(
-    val id: Long,
+    val id: Int,
     val name: String,
     val imageUrl: String
 )
