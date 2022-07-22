8d77b9a6ab9cbaadeb86c3952a033c67e8263882
Piotr Prus <prus.piotr@gmail.com>
2022-07-22 12:48:46 +0100

    create a homeViewModel that will have the logic for items

---
+++ app/src/main/java/com/piotrprus/marvelheroes/feature/home/HomeViewModel.kt
@@ -0,0 +1,26 @@
+package com.piotrprus.marvelheroes.feature.home
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import androidx.paging.Pager
+import androidx.paging.PagingConfig
+import androidx.paging.PagingData
+import androidx.paging.cachedIn
+import com.piotrprus.marvelheroes.ui.model.CharacterItem
+import kotlinx.coroutines.flow.Flow
+
+class HomeViewModel(
+    private val pagingSource: CharactersSource
+) : ViewModel() {
+
+    val pagedList: Flow<PagingData<CharacterItem>> =
+        Pager(PAGING_CONFIG) { pagingSource }.flow.cachedIn(viewModelScope)
+
+    companion object {
+        val PAGING_CONFIG = PagingConfig(
+            pageSize = 20,
+            initialLoadSize = 40
+        )
+    }
+
+}
