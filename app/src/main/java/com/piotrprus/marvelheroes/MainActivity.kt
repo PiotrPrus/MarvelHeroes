1d38e1596faae2b3db7b69ce16f658b2f33363a3
Piotr Prus <prus.piotr@gmail.com>
2022-07-22 12:49:19 +0100

    make a simple list, just to check if paging works properly

--- app/src/main/java/com/piotrprus/marvelheroes/MainActivity.kt
+++ app/src/main/java/com/piotrprus/marvelheroes/MainActivity.kt
@@ -4,13 +4,17 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.paging.compose.collectAsLazyPagingItems
+import androidx.paging.compose.items
+import com.piotrprus.marvelheroes.feature.home.HomeViewModel
 import com.piotrprus.marvelheroes.ui.theme.MarvelHeroesTheme
+import org.koin.androidx.compose.getViewModel
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -22,7 +26,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    SimpleTextList()
                 }
             }
         }
@@ -30,14 +34,14 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    MarvelHeroesTheme {
-        Greeting("Android")
+fun SimpleTextList() {
+    val viewModel = getViewModel<HomeViewModel>()
+    val lazyItems = viewModel.pagedList.collectAsLazyPagingItems()
+    LazyColumn {
+        items(lazyItems) { hero ->
+            hero?.let {
+                Text(text = "This is ${it.name}")
+            }
+        }
     }
 }
