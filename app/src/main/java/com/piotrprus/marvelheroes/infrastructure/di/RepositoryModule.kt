a2e95c927329b6741f042465b334ffae3c5a9841
Piotr Prus <prus.piotr@gmail.com>
2022-07-22 12:50:04 +0100

    Add CharactersSource to DI

--- app/src/main/java/com/piotrprus/marvelheroes/infrastructure/di/RepositoryModule.kt
+++ app/src/main/java/com/piotrprus/marvelheroes/infrastructure/di/RepositoryModule.kt
@@ -1,8 +1,10 @@
 package com.piotrprus.marvelheroes.infrastructure.di
 
+import com.piotrprus.marvelheroes.feature.home.CharactersSource
 import com.piotrprus.marvelheroes.infrastructure.repository.CharactersRepository
 import org.koin.dsl.module
 
 val repositoryModule = module {
     single<CharactersRepository> { CharactersRepository.Impl(get()) }
+    single { CharactersSource(get()) }
 }
